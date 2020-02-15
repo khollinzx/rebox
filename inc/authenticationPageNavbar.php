f469a21d18b72d2f535a73e6bd3a5c34d3a59f24
Khollinzx <collinsbenson0039@gmail.com>
2020-02-15 17:46:11 +0100

    added a new file authenticationPageNavbar for Login and Signup page

---
+++ inc/authenticationPageNavbar.php
@@ -0,0 +1,14 @@
+<!--header section start-->
+<header class="header">
+    <!--start navbar-->
+    <nav class="navbar navbar-expand-lg fixed-top bg-transparent">
+        <div class="container">
+            <a class="navbar-brand" href="index-2.html"><img src="<?php echo BASE_URL; ?>assets/img/Asset 7.png" width="120" alt="logo" class="img-fluid"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="ti-menu"></span>
+            </button>
+        </div>
+    </nav>
+    <!--end navbar-->
+</header>
+<!--header section end-->
